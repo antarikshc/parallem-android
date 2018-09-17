426bc589ec1a4bf447a345a95cc165e61522db2d
Antariksh C <antarikshc@gmail.com>
2018-09-17 16:02:10 +0530

    feat: Add LoginActivtiy - barebones
    Signed-off-by: Antariksh C <antarikshc@gmail.com>

--- app/src/main/java/com/antarikshc/parallem/activity/MainActivity.java
+++ app/src/main/java/com/antarikshc/parallem/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.antarikshc.parallem;
+package com.antarikshc.parallem.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+
+import com.antarikshc.parallem.R;
 
 public class MainActivity extends AppCompatActivity {
 
