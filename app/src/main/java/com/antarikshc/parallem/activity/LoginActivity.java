12760781bf098d7285d883bc7ddd68eb5252eb8a
Antariksh C <antarikshc@gmail.com>
2018-09-18 02:23:08 +0530

    feat: Add Form Validation in LoginActivity
    Signed-off-by: Antariksh C <antarikshc@gmail.com>

--- app/src/main/java/com/antarikshc/parallem/activity/LoginActivity.java
+++ app/src/main/java/com/antarikshc/parallem/activity/LoginActivity.java
@@ -4,7 +4,9 @@ import android.content.Context;
 import android.graphics.Rect;
 import android.os.Bundle;
 import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Patterns;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
@@ -13,8 +15,15 @@ import com.antarikshc.parallem.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    // Views
     private TextInputEditText editEmail;
+    private TextInputLayout editEmailLayout;
     private TextInputEditText editPassword;
+    private TextInputLayout editPasswordLayout;
+
+    // Global params
+    private String userEmail;
+    private String userPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,12 +39,62 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     /**
+     * OnClick Button Login - Validate form and Call API
+     *
+     * @param view
+     */
+    public void hitLoginAPI(View view) {
+
+        // Retrieve data from views
+        userEmail = editEmail.getText().toString();
+        userPassword = editPassword.getText().toString();
+
+        // Clear all errors
+        editEmailLayout.setError(null);
+        editPasswordLayout.setError(null);
+
+        if (validateForm()) {
+
+            // TODO: Call Login API
+
+        }
+    }
+
+    /**
+     * Validate form before Calling Login API
+     *
+     * @return Boolean validation
+     */
+    private Boolean validateForm() {
+
+        Boolean validation = true;
+
+        if (userEmail.isEmpty()) {
+            editEmailLayout.setError("*Required");
+            validation = false;
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(userEmail).matches()) {
+            editEmailLayout.setError("Input Valid Email");
+            validation = false;
+        }
+
+        if (userPassword.isEmpty()) {
+            editPasswordLayout.setError("*Required");
+            validation = false;
+        }
+
+        return validation;
+
+    }
+
+    /**
      * Boilerplate to Find and initialize all views
      */
     private void initializeViews() {
         editEmail = findViewById(R.id.edit_email);
         editEmail.requestFocus();
         editPassword = findViewById(R.id.edit_password);
+        editEmailLayout = findViewById(R.id.edit_email_layout);
+        editPasswordLayout = findViewById(R.id.edit_password_layout);
     }
 
     /**
