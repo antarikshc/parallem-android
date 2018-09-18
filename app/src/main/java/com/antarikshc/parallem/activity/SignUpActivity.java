c4c34afad2b2f70b2501da5c7f27b891c2140f03
Antariksh C <antarikshc@gmail.com>
2018-09-18 15:38:14 +0530

    feat: Touch outside to Hide keyb on SignUpActivity
    Signed-off-by: Antariksh C <antarikshc@gmail.com>

--- app/src/main/java/com/antarikshc/parallem/activity/SignUpActivity.java
+++ app/src/main/java/com/antarikshc/parallem/activity/SignUpActivity.java
@@ -1,20 +1,61 @@
 package com.antarikshc.parallem.activity;
 
+import android.content.Context;
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 
 import com.antarikshc.parallem.R;
 
 public class SignUpActivity extends AppCompatActivity {
 
+    // Views
+    private TextInputEditText editFirstName;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        initializeViews();
+
+    }
+
+    /**
+     * Boilerplate to Find and initialize all views
+     */
+    private void initializeViews() {
+        editFirstName = findViewById(R.id.edit_signup_firstname);
+        editFirstName.requestFocus();
     }
 
     public void backButton(View view) {
         onBackPressed();
+    }
+
+    /**
+     * Boilerplate: Tap outside to hide soft input manager
+     */
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            View v = getCurrentFocus();
+            if (v instanceof TextInputEditText) {
+                Rect outRect = new Rect();
+                v.getGlobalVisibleRect(outRect);
+                if (!outRect.contains((int) event.getRawX(), (int) event.getRawY())) {
+                    v.clearFocus();
+                    InputMethodManager imm = (InputMethodManager) getSystemService(
+                            Context.INPUT_METHOD_SERVICE);
+                    assert imm != null;
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                }
+            }
+        }
+        return super.dispatchTouchEvent(event);
     }
 }
