947eeaeb7d09534f7d26dd450a371fa4e7aad9aa
Antariksh C <antarikshc@gmail.com>
2018-10-02 02:53:05 +0530

    feat: Add Bottom Nav Item selected listener
    Signed-off-by: Antariksh C <antarikshc@gmail.com>

--- app/src/main/java/com/antarikshc/parallem/ui/dashboard/DashboardActivity.java
+++ app/src/main/java/com/antarikshc/parallem/ui/dashboard/DashboardActivity.java
@@ -1,16 +1,23 @@
 package com.antarikshc.parallem.ui.dashboard;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.MenuItem;
 
 import com.antarikshc.parallem.R;
 
 public class DashboardActivity extends AppCompatActivity {
 
+    public static final String LOG_TAG = DashboardActivity.class.getSimpleName();
+
     // Views
     private Toolbar toolbar;
+    private BottomNavigationView bottomNav;
 
     // Global params
     private FragmentTransaction fragmentTransaction;
@@ -20,12 +27,71 @@ public class DashboardActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
+        // Begin Fragment Transaction
         fragmentTransaction = getSupportFragmentManager().beginTransaction();
+
+        // Keep HomeFragment attached by default
         fragmentTransaction.add(R.id.frame_dash_main, new HomeFragment());
-        fragmentTransaction.addToBackStack("sign_up_phone");
+        fragmentTransaction.addToBackStack("home");
         fragmentTransaction.commit();
 
         setupToolbar();
+
+        setupBottomNavListner();
+    }
+
+    private void setupBottomNavListner() {
+
+        bottomNav = findViewById(R.id.bottom_nav_dashboard);
+
+        bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                switch (menuItem.getItemId()) {
+
+                    case R.id.bottom_nav_item_home:
+                        if (!menuItem.isChecked()) {
+                            Log.i(LOG_TAG, "Bottom Nav Item: Home");
+                            menuItem.setChecked(true);
+                        }
+                        return true;
+
+                    case R.id.bottom_nav_item_projects:
+                        if (!menuItem.isChecked()) {
+                            Log.i(LOG_TAG, "Bottom Nav Item: Projects");
+                            menuItem.setChecked(true);
+                        }
+                        return true;
+
+                    case R.id.bottom_nav_item_teams:
+                        if (!menuItem.isChecked()) {
+                            Log.i(LOG_TAG, "Bottom Nav Item: Teams");
+                            menuItem.setChecked(true);
+                        }
+                        return true;
+
+                    case R.id.bottom_nav_item_alerts:
+                        if (!menuItem.isChecked()) {
+                            Log.i(LOG_TAG, "Bottom Nav Item: Alerts");
+                            menuItem.setChecked(true);
+                        }
+                        return true;
+
+                    case R.id.bottom_nav_item_profile:
+                        if (!menuItem.isChecked()) {
+                            Log.i(LOG_TAG, "Bottom Nav Item: Profiles");
+                            menuItem.setChecked(true);
+                        }
+                        return true;
+
+                    default:
+                        return false;
+
+                }
+            }
+        });
+
     }
 
     /**
