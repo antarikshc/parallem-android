b469e9aae660dafe0508db75a6fdf3db05dfa6f2
Antariksh C <antarikshc@gmail.com>
2018-10-01 23:50:01 +0530

    feat: Add HomeFragment - With partial UI - Initiate on DashboardActivity - TODO: Bottom Nav Bar
    Signed-off-by: Antariksh C <antarikshc@gmail.com>

--- app/src/main/java/com/antarikshc/parallem/activity/DashboardActivity.java
+++ app/src/main/java/com/antarikshc/parallem/activity/DashboardActivity.java
@@ -1,20 +1,30 @@
 package com.antarikshc.parallem.activity;
 
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
 import com.antarikshc.parallem.R;
+import com.antarikshc.parallem.fragment.HomeFragment;
 
 public class DashboardActivity extends AppCompatActivity {
 
     // Views
     private Toolbar toolbar;
 
+    // Global params
+    private FragmentTransaction fragmentTransaction;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.add(R.id.frame_dash_main, new HomeFragment());
+        fragmentTransaction.addToBackStack("sign_up_phone");
+        fragmentTransaction.commit();
 
         setupToolbar();
     }
---
+++ app/src/main/java/com/antarikshc/parallem/fragment/HomeFragment.java
@@ -0,0 +1,36 @@
+package com.antarikshc.parallem.fragment;
+
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.antarikshc.parallem.R;
+import com.antarikshc.parallem.databinding.FragmentHomeBinding;
+
+
+public class HomeFragment extends Fragment {
+
+    // Global params
+    private FragmentHomeBinding binding;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        // Inflate Fragment layout with data binding
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false);
+
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+}
