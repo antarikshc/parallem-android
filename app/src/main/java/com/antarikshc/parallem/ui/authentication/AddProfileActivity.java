b385c0c2d74f0a95f00cafcfe75252c4fa08511c
Antariksh C <antarikshc@gmail.com>
2018-10-07 17:30:42 +0530

    feat: Add CareerDetails Fragment [no UI]
    Signed-off-by: Antariksh C <antarikshc@gmail.com>

--- app/src/main/java/com/antarikshc/parallem/ui/authentication/AddProfileActivity.java
+++ app/src/main/java/com/antarikshc/parallem/ui/authentication/AddProfileActivity.java
@@ -1,15 +1,64 @@
 package com.antarikshc.parallem.ui.authentication;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 
 import com.antarikshc.parallem.R;
 
 public class AddProfileActivity extends AppCompatActivity {
 
+    // Global params
+    private FragmentManager fragmentManager;
+    private FragmentTransaction fragmentTransaction;
+    private CareerDetailsFragment careerDetailsFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_profile);
+
+        careerDetailsFragment = new CareerDetailsFragment();
+
+        setupFragmentManager();
+
+    }
+
+    /**
+     * Setup Fragment Manager and Transactions
+     * Add ExperienceProject to backstack
+     */
+    private void setupFragmentManager() {
+        // Retrieve FragmentManager instance
+        fragmentManager = getSupportFragmentManager();
+
+        // Keep CareerDetailsFragment attached by default
+        attachFragment(careerDetailsFragment);
+    }
+
+
+    /**
+     * Method to avoid fragment recreation
+     *
+     * @param fragment Pass the Fragment to be launched
+     */
+    private void attachFragment(Fragment fragment) {
+        String backStateName = fragment.getClass().getSimpleName();
+
+        Fragment fragmentFromBackStack = getSupportFragmentManager().findFragmentByTag(backStateName);
+
+        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+
+        if (fragmentFromBackStack == null) {
+            // Fragment not in back stack, create it.
+            fragmentTransaction.replace(R.id.frame_add_profile, fragment, backStateName);
+            fragmentTransaction.addToBackStack(backStateName);
+            fragmentTransaction.commit();
+        } else {
+            fragmentManager.popBackStack(backStateName, 0);
+        }
     }
 }
---
+++ app/src/main/java/com/antarikshc/parallem/ui/authentication/CareerDetailsFragment.java
@@ -0,0 +1,34 @@
+package com.antarikshc.parallem.ui.authentication;
+
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.antarikshc.parallem.R;
+import com.antarikshc.parallem.databinding.FragmentCareerDetailsBinding;
+
+public class CareerDetailsFragment extends Fragment {
+
+    // Global params
+    private FragmentCareerDetailsBinding binding;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        // Inflate Fragment layout with data binding
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_career_details, container, false);
+
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
