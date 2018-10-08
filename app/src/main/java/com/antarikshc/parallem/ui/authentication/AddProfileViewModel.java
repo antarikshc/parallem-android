2fa22342a48c12f9de0616a601644ed2350d6842
Antariksh C <antarikshc@gmail.com>
2018-10-09 00:31:44 +0530

    feat: Fetch and Observe User signed up in ViewModel for ProfileDetails
    Signed-off-by: Antariksh C <antarikshc@gmail.com>

--- app/src/main/java/com/antarikshc/parallem/ui/authentication/AddProfileViewModel.java
+++ app/src/main/java/com/antarikshc/parallem/ui/authentication/AddProfileViewModel.java
@@ -1,10 +1,13 @@
 package com.antarikshc.parallem.ui.authentication;
 
 import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModel;
+import android.support.annotation.Nullable;
 
 import com.antarikshc.parallem.data.ParallemRepository;
 import com.antarikshc.parallem.models.Skill;
+import com.antarikshc.parallem.models.user.User;
 import com.antarikshc.parallem.ui.dashboard.DashboardViewModel;
 
 public class AddProfileViewModel extends ViewModel {
@@ -13,14 +16,31 @@ public class AddProfileViewModel extends ViewModel {
 
     private final ParallemRepository mRepository;
     private final LiveData<Skill[]> mSkills;
+    private User mUser;
 
     public AddProfileViewModel(ParallemRepository mRepository) {
         this.mRepository = mRepository;
         mSkills = mRepository.getAllSkills();
+        mRepository.getProfileDetails().observeForever(new Observer<User>() {
+            @Override
+            public void onChanged(@Nullable User user) {
+                mUser = user;
+            }
+        });
     }
 
     // Return the List of all Skills
     public LiveData<Skill[]> getAllSkills() {
         return mSkills;
+    }
+
+    // Update User
+    public void updateUser(User user) {
+        mUser = user;
+    }
+
+    // Retrieve user
+    public User getUser() {
+        return mUser;
     }
 }
