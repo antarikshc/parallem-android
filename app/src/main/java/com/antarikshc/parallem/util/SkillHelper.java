71f82feb3a64caa9802c498c6c158d0d7bfe8d18
Antariksh C <antarikshc@gmail.com>
2018-10-08 22:07:16 +0530

    feat: Add Helper class for maintain skill - It is very stupid. Do not look
    Signed-off-by: Antariksh C <antarikshc@gmail.com>

---
+++ app/src/main/java/com/antarikshc/parallem/util/SkillHelper.java
@@ -0,0 +1,40 @@
+package com.antarikshc.parallem.util;
+
+import android.util.Log;
+
+import com.antarikshc.parallem.models.Skill;
+
+public class SkillHelper {
+
+    // What am about to do is very stupid
+    // But am on a time constraint and implementing Custom AutoCompleteTextView and CustomAdapter
+    // is not doable right at the moment
+
+    public static Integer getSkillId(Skill[] skills, String inputSkill) {
+
+        Integer skillId = null;
+
+        for (Skill skill : skills) {
+
+            if (inputSkill.equals(skill.getName())) {
+                skillId = skill.getSkillId();
+            }
+
+        }
+
+        return skillId;
+    }
+
+    public static String[] getSkillNameArray(Skill[] skills) {
+
+        String[] skillNameArray = new String[skills.length];
+
+        for (int i = 0; i < skills.length; i++) {
+            Log.i("Skill", "Skill: " + skills[i].getName());
+            skillNameArray[i] = skills[i].getName();
+        }
+
+        return skillNameArray;
+    }
+
+}
