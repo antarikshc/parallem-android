500d8ba68b846257848c49e384d5ea7e566649ea
Antariksh C <antarikshc@gmail.com>
2018-10-12 11:15:29 +0530

    feat: Add UserProfileActivity with UI
    Signed-off-by: Antariksh C <antarikshc@gmail.com>

---
+++ app/src/main/java/com/antarikshc/parallem/ui/UserProfileActivity.java
@@ -0,0 +1,15 @@
+package com.antarikshc.parallem.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.antarikshc.parallem.R;
+
+public class UserProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile);
+    }
+}
