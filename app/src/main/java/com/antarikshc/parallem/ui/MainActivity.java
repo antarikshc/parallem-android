72dc0b9b1c865998cc5f4d2129a0f7955ab74f1e
Antariksh C <antarikshc@gmail.com>
2018-10-18 14:54:02 +0530

    refactor: Remove unused Login and SigUp Activity
    Signed-off-by: Antariksh C <antarikshc@gmail.com>

--- app/src/main/java/com/antarikshc/parallem/ui/MainActivity.java
+++ app/src/main/java/com/antarikshc/parallem/ui/MainActivity.java
@@ -2,21 +2,13 @@ package com.antarikshc.parallem.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 
 import com.antarikshc.parallem.R;
 import com.antarikshc.parallem.ui.authentication.AuthenticationActivity;
-import com.antarikshc.parallem.ui.authentication.LoginActivity;
-import com.antarikshc.parallem.ui.authentication.SignUpActivity;
 import com.antarikshc.parallem.ui.dashboard.DashboardActivity;
 import com.antarikshc.parallem.ui.team.TeamProfileActivity;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.iid.FirebaseInstanceId;
-import com.google.firebase.iid.InstanceIdResult;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,30 +16,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        FirebaseInstanceId.getInstance().getInstanceId()
-                .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<InstanceIdResult> task) {
-                        if (!task.isSuccessful()) {
-                            Log.w("Firebase Instance", "getInstanceId failed", task.getException());
-                            return;
-                        }
-                        // Get new Instance ID token
-                        String token = task.getResult().getToken();
-                        Log.i("Firebase Instance", "Token: " + token);
-                    }
-                });
-    }
-
-    public void goToLogin(View view) {
-        Intent intent = new Intent(this, LoginActivity.class);
-        startActivity(intent);
-    }
-
-    public void goToSignUp(View view) {
-        Intent intent = new Intent(this, SignUpActivity.class);
-        startActivity(intent);
     }
 
     public void goToDashboard(View view) {
@@ -55,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void goToAddProfile(View view) {
+    public void goToAuthentication(View view) {
         Intent intent = new Intent(this, AuthenticationActivity.class);
         startActivity(intent);
     }
